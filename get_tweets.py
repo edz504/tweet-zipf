0bfd3879ed23fbc4aaaa00b4a6c64bc6217e3bd1
edz504 <eddiedzhou@gmail.com>
2014-08-25 22:50:34 -0700

    separated FreqDist request into functions with parameters

--- get_tweets.py
+++ get_tweets.py
@@ -18,55 +18,62 @@ twitter_api = twitter.Api(
     access_token_key='431244726-M96XnETmwt7fZC3Dkbuy0jsUNOpFAjFZru4z4gCZ',
     access_token_secret='S4kbwMkUrZJfYmbsohaepnnbIMd1YfEZb4IaJoUQtDQ4a')
 
+def getUserTweetWordFreqDist(sn):
+    user = twitter_api.GetUser(screen_name=sn)
+
+    user_tweets = []
+    keep_em_coming = True
+
+    while (keep_em_coming):
+        # for our first time, no max_id
+        if len(user_tweets) == 0:
+            this_user_tweets = twitter_api.GetUserTimeline(
+                screen_name=sn,
+                count=200)
+        else:
+            this_user_tweets = twitter_api.GetUserTimeline(
+                screen_name=sn,
+                count=200,
+                max_id=oldest_id)
+        print 'Pulled in ' + str(len(this_user_tweets)) + ' new tweets'
+        if len(this_user_tweets) == 1:
+            keep_em_coming = False
+        oldest_id = min([t.id for t in this_user_tweets])
+        user_tweets += this_user_tweets
+        
+    user_tweets_str = [t.text.encode('utf-8') for t in user_tweets]
+
+    user_tweets_clean1 = [tweet_cleaner.remove_sn(t_str1) for t_str1 in user_tweets_str]
+
+    user_tweets_clean2 = [tweet_cleaner.remove_urls(t_str2) for t_str2 in user_tweets_clean1]
+
+    user_tweets_clean3 = [tweet_cleaner.leave_alphanumeric(t_str3) for t_str3 in user_tweets_clean2]
+
+    user_tweets_collapse = ''.join(user_tweets_clean3)
+    user_tweets_tokens = nltk.word_tokenize(user_tweets_collapse)
+
+    fdist = FreqDist(word.lower() for word in user_tweets_tokens)
+    return(fdist)
+
+def plotUserTopNWords(sn, n, fdist=None):
+    if fdist is None:
+        print 'Getting tweets for ' + sn
+        fdist = getUserTweetWordFreqDist(sn)
+    fsort_tuple = sorted(fdist.items(), key=operator.itemgetter(1),
+        reverse=True)
+    freqs_np_vals = np.array([t[1] for t in fsort_tuple])[0:n]
+    freqs_np_words = np.array([t[0] for t in fsort_tuple])[0:n]
+    width = .35
+    ind = np.arange(len(freqs_np_vals))
+    plt.title('Ranked Word Counts in the Tweets of ' + sn)
+    plt.bar(ind, freqs_np_vals)
+    plt.xticks(ind + width / 2, freqs_np_words, rotation='vertical')
+    mng = plt.get_current_fig_manager()
+    mng.window.state('zoomed')
+    plt.show()
+
+
 sn = 'edz504'
-user = twitter_api.GetUser(screen_name=sn)
+NUM_PLOT = 80
+plotUserTopNWords(sn=sn, n=NUM_PLOT, fdist = fdist)
 
-user_tweets = []
-keep_em_coming = True
-
-while (keep_em_coming):
-    # for our first time, no max_id
-    if len(user_tweets) == 0:
-        this_user_tweets = twitter_api.GetUserTimeline(
-            screen_name=sn,
-            count=200)
-    else:
-        this_user_tweets = twitter_api.GetUserTimeline(
-            screen_name=sn,
-            count=200,
-            max_id=oldest_id)
-    print 'Pulled in ' + str(len(this_user_tweets)) + ' new tweets'
-    if len(this_user_tweets) == 1:
-        keep_em_coming = False
-    oldest_id = min([t.id for t in this_user_tweets])
-    user_tweets += this_user_tweets
-    
-user_tweets_str = [t.text.encode('utf-8') for t in user_tweets]
-
-user_tweets_clean1 = [tweet_cleaner.remove_sn(t_str1) for t_str1 in user_tweets_str]
-
-user_tweets_clean2 = [tweet_cleaner.remove_urls(t_str2) for t_str2 in user_tweets_clean1]
-
-user_tweets_clean3 = [tweet_cleaner.leave_alphanumeric(t_str3) for t_str3 in user_tweets_clean2]
-
-# user_tweets_clean = [tweet_cleaner.leave_alphanumeric(
-#     tweet_cleaner.remove_urls(
-#         tweet_cleaner.remove_sn(t) for t in user_tweets_str))]
-
-
-user_tweets_collapse = ''.join(user_tweets_clean3)
-user_tweets_tokens = nltk.word_tokenize(user_tweets_collapse)
-
-fdist = FreqDist(word.lower() for word in user_tweets_tokens)
-fsort_tuple = sorted(fdist.items(), key=operator.itemgetter(1),
-    reverse=True)
-
-NUM_PLOT = 40
-freqs_np_vals = np.array([t[1] for t in fsort_tuple])[0:NUM_PLOT]
-freqs_np_words = np.array([t[0] for t in fsort_tuple])[0:NUM_PLOT]
-
-width = .35
-ind = np.arange(len(freqs_np_vals))
-plt.bar(ind, freqs_np_vals)
-plt.xticks(ind + width / 2, freqs_np_words, rotation='vertical')
-plt.show()
