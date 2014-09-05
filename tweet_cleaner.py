c0d3fe9f2440c96383a8086fb1d0ab4b271a5037
edz504 <eddiedzhou@gmail.com>
2014-09-04 20:30:53 -0700

    tweet cleaning and retrieving embedded in ipython notebook

--- tweet_cleaner.py
+++ tweet_cleaner.py
@@ -7,7 +7,7 @@ def remove_sn(t):
         ats_cleaned = False
     while(not ats_cleaned):
         at_ind_start = t.index('@')
-        # the space should really be any non-screen-name-enabled character
+        # any non-screen-name-enabled character
         # so if the tweet is "...@Beyonce:blahhh "
         # then we leave blahhh in the string too
         m = re.search(r'\W', t[at_ind_start + 1:])
@@ -25,12 +25,14 @@ def remove_sn(t):
             break
         rep += 1
     return(t)
+
 # now let's remove urls (they're definitely not words)
 def remove_urls(t):
     urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', t)
     for url in urls:
         t = re.sub(re.escape(url), '', t)
     return(t)
+    
 # now that we have taken care of the screen_names with the @'s, as well as
 # the URLs, we can remove all punctuation.  note that this leaves hashtags
 # (words that follow the # sign) in the string, because we believe this 
--- tweet_retriever.py
+++ tweet_retriever.py
@@ -8,7 +8,7 @@ import nltk
 import operator
 from nltk.probability import FreqDist
 
-import tweet_cleaner
+from tweet_cleaner import remove_sn, remove_urls, leave_alphanumeric
 import pickle
 import ast
 import datetime
@@ -23,10 +23,10 @@ twitter_api = twitter.Api(
     access_token_secret=mine[3])
 
 def getUserTweets(u):
-    if (u.statuses_count < 1): # don't bother going through if we know
-    # the user doesn't have any tweets
+    if (u.statuses_count < 1 or u.protected): # don't bother going through if we know the user doesn't have any tweets
+        # or if they're protected
         return([])
-    if (not u.protected):
+    else (not u.protected):
         user_tweets = []
         keep_em_coming = True
 
@@ -50,8 +50,7 @@ def getUserTweets(u):
             user_tweets += this_user_tweets
 
         return(user_tweets)
-    else: # return empty list for protected users
-        return([])
+
 
 # supply either a screen name, or a list of tweets
 def getUserTweetWordFreqDist(u, user_tweets=None):
@@ -59,12 +58,7 @@ def getUserTweetWordFreqDist(u, user_tweets=None):
         user_tweets = getUserTweets(u)
     user_tweets_str = [t.text.encode('utf-8') for t in user_tweets]
     user_tweets_clean1 = [tweet_cleaner.remove_sn(t_str1) for t_str1 in user_tweets_str]
-    
-    # bug here with specifics:
-    # 'So funny! I want to listen to this again and again... ( http://t.co/Fwyw0He8)'
     user_tweets_clean2 = [tweet_cleaner.remove_urls(t_str2) for t_str2 in user_tweets_clean1]
-    #
-
     user_tweets_clean3 = [tweet_cleaner.leave_alphanumeric(t_str3) for t_str3 in user_tweets_clean2]
     user_tweets_collapse = ''.join(user_tweets_clean3)
     user_tweets_tokens = nltk.word_tokenize(user_tweets_collapse)
