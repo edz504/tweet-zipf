105dbf81d132cef276c0f31d0627c7354ba4320e
edz504 <eddiedzhou@gmail.com>
2014-10-04 16:54:13 -0400

    finished notebook, need to run code + output for cell order

--- map_cities.R
+++ map_cities.R
@@ -16,26 +16,13 @@ sq.mi <- scale(cities.df$sq_mi)
 us.map + geom_point(data=cities.df, 
     aes(x = lng, y=lat, colour=lex_div),
     size= REG_SIZE + sq.mi) + 
-    scale_colour_gradient(low="white", high="#3498db")
+    scale_colour_gradient(low="#16a085", high="#f39c12")
+ggsave(file="lex.png", width=8, height=4.5)
 
 # color by zipfian fit
 us.map + geom_point(data=cities.df, 
     aes(x = lng, y=lat, color=zipf_fit),
     size= REG_SIZE + sq.mi) + 
-    scale_colour_gradient(low="white", high="#c0392b")
+    scale_colour_gradient(low="#27ae60", high="#d35400")
+ggsave(file="zf.png", width=8, height=4.5)
 
-uq <- unique(cities.df$top_word)
-topToInt <- function(word) {
-    return(which(word == uq))
-}
-
-cities.df <- cities.df %>% 
-    mutate(top_int=sapply(cities.df$top_word, topToInt))
-
-REG_SIZE2 <- 50
-# color by most common word
-us.map + geom_point(data=cities.df, 
-    aes(x = lng, y=lat, 
-        colour=factor(cities.df$top_int),
-        size= REG_SIZE2)) + 
-    guides(colour = guide_legend(override.aes = list(alpha = 1)))
